4e2973702a4aaac83762bc6d7b419f7d0921e084
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-03-21 16:31:20 +0100

    add attribut class FaceSnapComponent

---
+++ src/app/face-snap/face-snap.component.ts
@@ -0,0 +1,15 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-face-snap',
+  templateUrl: './face-snap.component.html',
+  styleUrls: ['./face-snap.component.scss']
+})
+
+//les propriétés de class et les methodes ne necessite pas de mot clé var let const ou function 
+export class FaceSnapComponent {
+  title: string;
+  description: string
+  createDate: Date;
+  snaps: number;
+}
