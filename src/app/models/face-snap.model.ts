0bbbf660817b639105d7b4f88cf6d9336ff970b0
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-03-24 13:48:56 +0100

    app-structure-dynamic: face-snap.model.ts=> delete constructor , add property optional with ?

--- src/app/models/face-snap.model.ts
+++ src/app/models/face-snap.model.ts
@@ -27,9 +27,23 @@ export class FaceSnap {
         // il s agit d un raccourci au lieu du code ci dessus, mais comme les proprités ne sont pas declaré dans la classe 
         //qui par defaut est public, nous ajoutons le modificateur d acces "public"et nous indiquons a typescript dans les parametres du constructor de class que ces propriétés sont public 
         //sans ce modificateur, en appelant le constructor avec new, les parametres de la fonction constructor (les propriétés) ne sont pas accessible depuis l exterieur de la classe 
-        constructor(public title: string, 
+        
+        /*constructor(public title: string, 
             public description: string,
             public createDate: Date,
             public snaps: number,
-            public imageUrl: string) {}
+            public imageUrl: string) {}*/
+
+        /*REFACTORING: de la class sans le constructor qui comporte trop d argument et ajout de propriétés optionnelles avec le point d interrogation*/
+
+        //on ajoute a nouveau le bang ! pour promettre a typescript que nous allons initialiser les valeurs car plus de constructor pour initialiser les propriété
+            title!: string;
+            description!: string;
+            createDate!: Date;
+            snaps!: number;
+            imageUrl!: string;
+            location?: string; 
+            //propriétés optionnelles qui ne sera pas une propriété requise qui provoque une erreur si elle n est pas indiqué dans l 'objet facesnap que nous allons recree mais sans instance(plus de constrcutor)
+            //la class sans le constructor sera une class type pour les objets facesnap que nous allé crée a partir de ce type class
+
 }
