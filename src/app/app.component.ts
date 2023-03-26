d3ab005dea96c58641534b5808caac2e7d521ab1
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-03-26 17:49:34 +0200

    style: delate white space

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,50 +6,52 @@ import { FaceSnap} from './models/face-snap.model';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent implements OnInit {
-  snap!: FaceSnap;// la classe est un type de donnée,
-  
-  // donc on le declare en Type pour ts et l intialiser avec ng OnInit avec les valeur de l instance de la classe crée avecd new
+  mySnaps!: FaceSnap[];
+  // la classe est un type de donnée, s ajoute un tableau de type class, un tableau qui utlise le type class FaceSnap dans le principe du mot clé interface a la place de class pour le model FaceSnap
   ngOnInit() {
-    this.snap = {
-      title: "test titre class",
-      description: "description class model",
-      createDate: new Date(), 
-      snaps: 1,
-      imageUrl: "../../assets/book.jpg",
-      location: "Paris"
-    }
-    console.log("snap instance model",this.snap);
-    this.snap2 = {
-      title: "2eme instance",
-      description: "description test 2eme instance",
-      createDate: new Date(), 
-      snaps:5, 
-      imageUrl: "../../assets/book-antique.jpg",  
-       
-    }
-    this.snap3 = {
-      title:"3eme instance", 
-      description:"description test 3eme instance",
-      createDate: new Date(), 
-      snaps:3, 
-      imageUrl: "../../assets/books.jpg",
-      location: "Paris"
-    }
-    this.snap4 = {
-      title: "4eme instance", 
-      description:"description test 4eme instance",
-      createDate: new Date(), 
-      snaps:0, 
-      imageUrl:"../../assets/book-library.jpg",
-      location: "montagne" 
-    }
-    this.snap5 = {
-      title:"description test 5eme instance",
-      description:"description test 5me instance",
-      createDate: new Date(), 
-      snaps:5, 
-      imageUrl:"../../assets/book-desk.jpg" 
-    }
-  }
+    this.mySnaps = [
+      {
+        title: "test titre class",
+        description: "description class model",
+        createDate: new Date(), 
+        snaps: 1,
+        imageUrl: "../../assets/book.jpg",
+        location: "Paris"
+      },
+      {
+        title: "2eme instance",
+        description: "description test 2eme instance",
+        createDate: new Date(), 
+        snaps: 5, 
+        imageUrl: "../../assets/book-antique.jpg",  
+         
+      },
+      {
+        title:"3eme instance", 
+        description:"description test 3eme instance",
+        createDate: new Date(), 
+        snaps: 3, 
+        imageUrl: "../../assets/books.jpg",
+        location: "Paris"
+      },
+      {
+        title: "4eme instance", 
+        description:"description test 4eme instance",
+        createDate: new Date(), 
+        snaps: 0, 
+        imageUrl:"../../assets/book-library.jpg",
+        location: "montagne" 
+      },
+      {
+        title:"description test 5eme instance",
+        description:"description test 5me instance",
+        createDate: new Date(), 
+        snaps: 5, 
+        imageUrl:"../../assets/book-desk.jpg" 
+      }
+    ]
+    console.log("snap model",this.mySnaps);
+  }  
 }
