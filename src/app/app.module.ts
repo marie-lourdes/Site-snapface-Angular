f29b2dd61b33f08b88a210ef53f9dac8f12c3c73
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-03-28 11:22:45 +0200

    change locale data: refactoring app.module.ts and import LOCALE_ID, locale fr-FR , the methode registerLocaleData to change datas application in french

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,8 +1,12 @@
-import { NgModule } from '@angular/core';
+//import de LOCALE_ID
+import { LOCALE_ID, NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { FaceSnapComponent } from './face-snap/face-snap.component';
+//import de la methode registerlocaleData et de la local 'fr'
+import { registerLocaleData } from '@angular/common';
+import * as fr from '@angular/common/locales/fr';
 
 @NgModule({
   declarations: [
@@ -12,7 +16,15 @@ import { FaceSnapComponent } from './face-snap/face-snap.component';
   imports: [
     BrowserModule
   ],
-  providers: [],
+  providers: [
+    { provide: LOCALE_ID, useValue: 'fr-FR'} // Inplementation dans le decorateur ngModule de LOCALE_ID et de la valeur de la locale fr-FR
+  ],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+
+//Modification du AppModule avec le constructor et la methode registerLocaleDate avec pour argument la locale fr et sa propriété par defaut
+export class AppModule {
+  constructor() {
+    registerLocaleData(fr.default);
+  }
+ }
