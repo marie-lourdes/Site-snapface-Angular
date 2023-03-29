780713cd69d507581c16c49b1a4e0733b71d25c3
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-03-29 16:21:41 +0200

    restructuration application: move instance facenapComponent app-face-snap to the template face-snap-list.component.html

--- src/app/face-snap-list/face-snap-list.component.ts
+++ src/app/face-snap-list/face-snap-list.component.ts
@@ -6,7 +6,7 @@ import { FaceSnap} from '../models/face-snap.model';
   templateUrl: './face-snap-list.component.html',
   styleUrls: ['./face-snap-list.component.scss']
 })
-export class FaceSnapListComponent {
+export class FaceSnapListComponent implements OnInit {
   mySnaps!: FaceSnap[];
   // la classe est un type de donnée, s ajoute un tableau de type class, un tableau qui utlise le type class FaceSnap dans le principe du mot clé interface a la place de class pour le model FaceSnap
   ngOnInit() {
