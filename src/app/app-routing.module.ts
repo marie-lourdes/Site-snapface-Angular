1dff6a441914a4cc3bcec867af552b89733d03a3
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-04-03 19:42:19 +0200

    RouteLandingPage: app-routing.module.ts => add route empty for Home, the LandingPageComponent in AppRoutingModule

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import {NgModule} from "@angular/core";
 import {Routes, RouterModule} from "@angular/router";
 
 import {FaceSnapListComponent} from "./face-snap-list/face-snap-list.component";
+import { LandingPageComponent } from "./landing-page/landing-page.component";
 
 //declaration et initialisation des routes avec le path et les components à afficher avec le type d objet Routes
 const routes: Routes = [
-    {path:"facesnaps", component: FaceSnapListComponent}
+    {path:"facesnaps", component: FaceSnapListComponent},
+    {path: "", component: LandingPageComponent} //route vide avec le LandingPageComponent, home, accueil qui dirige vers les facesnaps
 ]
 
 // import du RouterModule pour le  configurer à la racine de l application avec les routes declaré dans le tableau routes ci dessus
