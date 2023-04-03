efa6f39b3dbcd224a25e55af8cf486113722d11e
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-04-03 16:27:29 +0200

    AppRoutingModule: app-routing.module.ts => add and configure RouterModule with routes in root of application in @NgModule with imports et exports

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,18 +3,21 @@ import {Routes, RouterModule} from "@angular/router";
 
 import {FaceSnapListComponent} from "./face-snap-list/face-snap-list.component";
 
-//declaration des routes avec le path et les components à afficher
-
+//declaration des routes avec le path et les components à afficher avec l objet Routes
 const routes: Routes = [
     {path:"facesnaps", component: FaceSnapListComponent}
 ]
 
-// import du RouterModule pour le  configurer à la racine de l application avec les routes derclaré dans le tableau routes ci dessus
+// import du RouterModule pour le  configurer à la racine de l application avec les routes declaré dans le tableau routes ci dessus
 //et( reexporte du RouterModule configuré)
 @NgModule({
+    imports:[
+        RouterModule.forRoot(routes)// configuration du RouterModule et on enregistre les routes à la racine
+    ],
+    exports: [
+        RouterModule
+    ]
 
 })
 
-export class AppRoutingModule {
-
-}
+export class AppRoutingModule {}
