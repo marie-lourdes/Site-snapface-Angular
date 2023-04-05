5ab956011163b58ac96d03f297a71eea6f20ef02
marie-lourdes <marielourdesleperlier64@gmail.com>
2023-04-05 10:36:21 +0200

    data facensnaps: move facesnaps in file data  for data for eventual server back end

---
+++ src/app/data/face-snaps.ts
@@ -0,0 +1,52 @@
+import { FaceSnap } from "../models/face-snap.model";
+
+export const faceSnaps: FaceSnap[]= [
+    {
+      id: 1,
+      title: "test titre class",
+      description: "description class model",
+      createDate: new Date(), 
+      snaps: 0,
+      imageUrl: "../../assets/book.jpg",
+      location: "Paris",
+      price: 56.45489465
+    },
+    {
+      id: 2,
+      title: "2eme instance",
+      description: "description test 2eme instance",
+      createDate: new Date(), 
+      snaps: 0, 
+      imageUrl: "../../assets/book-antique.jpg", 
+      price: 47.895356 
+       
+    },
+    {
+      id: 3,
+      title:"3eme instance", 
+      description:"description test 3eme instance",
+      createDate: new Date(), 
+      snaps: 0, 
+      imageUrl: "../../assets/books.jpg",
+      location: "Paris",
+      price: 798865.065595
+
+    },
+    {
+      id: 4,
+      title: "4eme instance", 
+      description:"description test 4eme instance",
+      createDate: new Date(), 
+      snaps: 0, 
+      imageUrl:"../../assets/book-library.jpg",
+      location: "montagne" 
+    },
+    {
+      id: 5,
+      title:"description test 5eme instance",
+      description:"description test 5me instance",
+      createDate: new Date(), 
+      snaps: 0, 
+      imageUrl:"../../assets/book-desk.jpg" 
+    }
+]
--- src/app/services/face-snaps.service.ts
+++ src/app/services/face-snaps.service.ts
@@ -1,6 +1,7 @@
 import {Injectable} from '@angular/core'; 
 // deuxieme alternative plus recent import de la fonction "inject", avec la creation non pas d une class mais d une fonction comme service qui apelle inject()
 import {FaceSnap} from '../models/face-snap.model'
+import { faceSnaps } from "../data/face-snaps";
 
 // Le decorateur declare la classe comme etant un service, specifiant que le service est enregistré à la racine de l application
 @Injectable({
@@ -11,56 +12,7 @@ import {FaceSnap} from '../models/face-snap.model'
 // on declare et on initialise le service dans la meme expression sans le bang ! qui promet une initialisation ulterieur
 export class FaceSnapsService {
   //centralisation des données FaceSnaps dans le service
-    mySnaps: FaceSnap[] = [
-        {
-          id: 1,
-          title: "test titre class",
-          description: "description class model",
-          createDate: new Date(), 
-          snaps: 0,
-          imageUrl: "../../assets/book.jpg",
-          location: "Paris",
-          price: 56.45489465
-        },
-        {
-          id: 2,
-          title: "2eme instance",
-          description: "description test 2eme instance",
-          createDate: new Date(), 
-          snaps: 0, 
-          imageUrl: "../../assets/book-antique.jpg", 
-          price: 47.895356 
-           
-        },
-        {
-          id: 3,
-          title:"3eme instance", 
-          description:"description test 3eme instance",
-          createDate: new Date(), 
-          snaps: 0, 
-          imageUrl: "../../assets/books.jpg",
-          location: "Paris",
-          price: 798865.065595
-    
-        },
-        {
-          id: 4,
-          title: "4eme instance", 
-          description:"description test 4eme instance",
-          createDate: new Date(), 
-          snaps: 0, 
-          imageUrl:"../../assets/book-library.jpg",
-          location: "montagne" 
-        },
-        {
-          id: 5,
-          title:"description test 5eme instance",
-          description:"description test 5me instance",
-          createDate: new Date(), 
-          snaps: 0, 
-          imageUrl:"../../assets/book-desk.jpg" 
-        }
-    ]
+    mySnaps: FaceSnap[] = faceSnaps;
 
     //centralisation des methodes relatives au données FaceSnaps dans le service
     // creation de la methode pour recuperer les données facesnap a utiliser dans les compoents qui en ont besoin tel que FaceSnapListComponent
